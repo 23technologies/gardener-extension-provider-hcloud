1b2b880169a156d7086555ca8aa2dc884f68017f
Tobias Wolf <wolf@23technologies.cloud>
2021-04-16 09:33:13 +0200

    Add support to remove worker networks

--- pkg/controller/infrastructure/actuator.go
+++ pkg/controller/infrastructure/actuator.go
@@ -16,7 +16,11 @@ package infrastructure
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud"
+	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis"
+	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis/helper"
 	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis/transcoder"
 	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis/v1alpha1"
 	"github.com/gardener/gardener/extensions/pkg/controller"
@@ -37,12 +41,57 @@ type actuator struct {
 	gardenID string
 }
 
+type actuatorConfig struct {
+	cloudProfileConfig *apis.CloudProfileConfig
+	infraConfig        *apis.InfrastructureConfig
+	region             *apis.RegionSpec
+	token              string
+}
+
 // NewActuator creates a new Actuator that updates the status of the handled Infrastructure resources.
 func NewActuator(gardenID string) infrastructure.Actuator {
 	return &actuator{
 		logger:   log.Log.WithName("infrastructure-actuator"),
 		gardenID: gardenID,
 	}
+}
+
+func (a *actuator) getActuatorConfig(ctx context.Context, infra *extensionsv1alpha1.Infrastructure, cluster *controller.Cluster) (*actuatorConfig, error) {
+	cloudProfileConfig, err := transcoder.DecodeCloudProfileConfigFromControllerCluster(cluster)
+	if err != nil {
+		return nil, err
+	}
+
+	infraConfig, err := transcoder.DecodeInfrastructureConfigFromInfrastructure(infra)
+	if err != nil {
+		return nil, err
+	}
+
+	region := helper.FindRegion(infra.Spec.Region, cloudProfileConfig)
+	if region == nil {
+		return nil, fmt.Errorf("Region %q not found in cloud profile", infra.Spec.Region)
+	}
+
+	secret, err := controller.GetSecretByReference(ctx, a.Client(), &infra.Spec.SecretRef)
+	if err != nil {
+		return nil, err
+	}
+
+	credentials, err := hcloud.ExtractCredentials(secret)
+	if err != nil {
+		return nil, err
+	}
+
+	token := credentials.HcloudCCM().HcloudToken
+
+	config := &actuatorConfig{
+		cloudProfileConfig: cloudProfileConfig,
+		infraConfig:        infraConfig,
+		region:             region,
+		token:              token,
+	}
+
+	return config, nil
 }
 
 func (a *actuator) Reconcile(ctx context.Context, config *extensionsv1alpha1.Infrastructure, cluster *controller.Cluster) error {
--- pkg/controller/infrastructure/actuator_reconcile.go
+++ pkg/controller/infrastructure/actuator_reconcile.go
@@ -18,79 +18,40 @@ package infrastructure
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/controller/infrastructure/ensurer"
-	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud"
 	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis"
-	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis/helper"
-	"github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis/transcoder"
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 )
 
-type preparedReconcile struct {
-	cloudProfileConfig *apis.CloudProfileConfig
-	infraConfig        *apis.InfrastructureConfig
-	region             *apis.RegionSpec
-	token              string
-}
-
-func (a *actuator) prepareReconcile(ctx context.Context, infra *extensionsv1alpha1.Infrastructure, cluster *extensionscontroller.Cluster) (*preparedReconcile, error) {
-	cloudProfileConfig, err := transcoder.DecodeCloudProfileConfigFromControllerCluster(cluster)
-	if err != nil {
-		return nil, err
-	}
-
-	infraConfig, err := transcoder.DecodeInfrastructureConfigFromInfrastructure(infra)
-	if err != nil {
-		return nil, err
-	}
-
-	region := helper.FindRegion(infra.Spec.Region, cloudProfileConfig)
-	if region == nil {
-		return nil, fmt.Errorf("Region %q not found in cloud profile", infra.Spec.Region)
-	}
-
-	secret, err := extensionscontroller.GetSecretByReference(ctx, a.Client(), &infra.Spec.SecretRef)
-	if err != nil {
-		return nil, err
-	}
-
-	credentials, err := hcloud.ExtractCredentials(secret)
-	if err != nil {
-		return nil, err
-	}
-
-	token := credentials.HcloudCCM().HcloudToken
-
-	prepared := &preparedReconcile{
-		cloudProfileConfig: cloudProfileConfig,
-		infraConfig:        infraConfig,
-		region:             region,
-		token:              token,
-	}
-
-	return prepared, nil
-}
-
 func (a *actuator) reconcile(ctx context.Context, infra *extensionsv1alpha1.Infrastructure, cluster *extensionscontroller.Cluster) error {
-	prepared, err := a.prepareReconcile(ctx, infra, cluster)
+	actuatorConfig, err := a.getActuatorConfig(ctx, infra, cluster)
 	if err != nil {
 		return err
 	}
 
-	client := apis.GetClientForToken(string(prepared.token))
+	client := apis.GetClientForToken(string(actuatorConfig.token))
 
 	err = ensurer.EnsureSSHPublicKey(ctx, client, infra.Spec.SSHPublicKey)
 	if err != nil {
 		return err
 	}
 
-	err = ensurer.EnsureNetworks(ctx, client, infra.Namespace, prepared.infraConfig.Networks)
+	err = ensurer.EnsureNetworks(ctx, client, infra.Namespace, actuatorConfig.infraConfig.Networks)
 	if err != nil {
 		return err
 	}
+/*
+	opts := hcloudclient.ImageListOpts{
+		IncludeDeprecated: true,
+	}
 
+	images, _, err := client.Image.List(ctx, opts)
+	if err != nil {
+		return err
+	}
+	panic(images)
+*/
 	return a.updateProviderStatus(ctx, infra)
 }
