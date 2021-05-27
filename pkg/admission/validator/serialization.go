61557f2e0a50378fbf1be1f5919914323d7f1570
Daniel Poelzleithner <poelzleithner@23technologies.cloud>
2021-05-27 12:25:44 +0200

    Start admission controller

---
+++ pkg/admission/validator/serialization.go
@@ -0,0 +1,66 @@
+// Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package validator
+
+import (
+	hcloud "github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis"
+
+	"github.com/gardener/gardener/extensions/pkg/util"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+// func decodeWorkerConfig(decoder runtime.Decoder, worker *runtime.RawExtension, fldPath *field.Path) (*hcloud.WorkerConfig, error) {
+// 	workerConfig := &hcloud.WorkerConfig{}
+// 	if err := util.Decode(decoder, worker.Raw, workerConfig); err != nil {
+// 		return nil, field.Invalid(fldPath, string(worker.Raw), "isn't a supported version")
+// 	}
+
+// 	return workerConfig, nil
+// }
+
+// func decodeControlPlaneConfig(decoder runtime.Decoder, cp *runtime.RawExtension, fldPath *field.Path) (*hcloud.ControlPlaneConfig, error) {
+// 	controlPlaneConfig := &hcloud.ControlPlaneConfig{}
+// 	if err := util.Decode(decoder, cp.Raw, controlPlaneConfig); err != nil {
+// 		return nil, field.Invalid(fldPath, string(cp.Raw), "isn't a supported version")
+// 	}
+
+// 	return controlPlaneConfig, nil
+// }
+
+func decodeInfrastructureConfig(decoder runtime.Decoder, infra *runtime.RawExtension, fldPath *field.Path) (*hcloud.InfrastructureConfig, error) {
+	infraConfig := &hcloud.InfrastructureConfig{}
+	if err := util.Decode(decoder, infra.Raw, infraConfig); err != nil {
+		return nil, field.Invalid(fldPath, string(infra.Raw), "isn't a supported version")
+	}
+
+	return infraConfig, nil
+}
+
+func decodeCloudProfileConfig(decoder runtime.Decoder, config *runtime.RawExtension) (*hcloud.CloudProfileConfig, error) {
+	cloudProfileConfig := &hcloud.CloudProfileConfig{}
+	if err := util.Decode(decoder, config.Raw, cloudProfileConfig); err != nil {
+		return nil, err
+	}
+	return cloudProfileConfig, nil
+}
+
+func decodeWorkerStatus(decoder runtime.Decoder, config *runtime.RawExtension) (*hcloud.WorkerStatus, error) {
+	cloudProfileConfig := &hcloud.WorkerStatus{}
+	if err := util.Decode(decoder, config.Raw, cloudProfileConfig); err != nil {
+		return nil, err
+	}
+	return cloudProfileConfig, nil
+}
