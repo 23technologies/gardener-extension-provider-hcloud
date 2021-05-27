cf849f145eea157aad8c628d8933fb45abc2d75e
Tobias Wolf <wolf@23technologies.cloud>
2021-05-27 17:56:58 +0200

    Fix "imported and not used" error in controlplane controller

--- pkg/controller/controlplane/valuesprovider.go
+++ pkg/controller/controlplane/valuesprovider.go
@@ -33,7 +33,6 @@ import (
 	"github.com/gardener/gardener/extensions/pkg/controller/controlplane/genericactuator"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
-	"github.com/gardener/gardener/pkg/utils"
 	"github.com/gardener/gardener/pkg/utils/chart"
 	k8sutils "github.com/gardener/gardener/pkg/utils/kubernetes"
 	"github.com/gardener/gardener/pkg/utils/secrets"
