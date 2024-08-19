b4f9e546957ab24779b5f41845b25e4bf35078b4
Jan Lohage <lohage@23technologies.cloud>
2024-08-19 16:34:06 +0200

    add shoot creation timeout of 25 minutes

--- hack/ci/03-test-shoot.sh
+++ hack/ci/03-test-shoot.sh
@@ -9,10 +9,21 @@ kind get kubeconfig -n gardener-extensions > gardener-kind.yaml
 export KUBECONFIG=gardener-kind.yaml
 yq '.metadata.name=env(TEST_SHOOT_NAME) | .spec.kubernetes.version=env(TEST_SHOOT_VERSION)' hack/ci/misc/test-shoot.yaml | kubectl apply -f -
 
+MAX_TRIES=300
+TRY=0
+WAIT=5
+
 echo "Waiting for shoot creation..."
 while [ ! "$(kubectl get shoot -n garden-project-1 "$TEST_SHOOT_NAME" -o jsonpath="{.status.lastOperation.state}")" == "Succeeded" ]; do
+  (( TRY+=1 ))
+
+  if [[ $TRY -gt $MAX_TRIES ]]; then
+      echo "Shoot creation timed out after $((WAIT * MAX_TRIES)) seconds"
+      exit 1;
+  fi
+
   PERCENTAGE=$(kubectl get shoot -n garden-project-1 "$TEST_SHOOT_NAME" -o jsonpath="{.status.lastOperation.progress}")
   echo "Creating shoot: $PERCENTAGE%"
-  sleep 5
+  sleep $WAIT
 done
 echo "Shoot creation succeeded"
