2e9d43dd76a873190937ea37d8572b587aaf14bb
Lothar Bach <bach@23technologies.cloud>
2025-03-07 14:56:37 +0100

    Revendor g/g v1.107.0 and support k8s v1.31+

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -1,18 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
-# Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved. This file is licensed under the Apache Software License, v. 2 except as noted otherwise in the LICENSE file
+# SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 
 set -o errexit
 set -o nounset
@@ -22,25 +12,17 @@ set -o pipefail
 source "$GARDENER_HACK_DIR"/vgopath-setup.sh
 
 CODE_GEN_DIR=$(go list -m -f '{{.Dir}}' k8s.io/code-generator)
-
-# We need to explicitly pass GO111MODULE=off to k8s.io/code-generator as it is significantly slower otherwise,
-# see https://github.com/kubernetes/code-generator/issues/100.
-export GO111MODULE=off
+source "${CODE_GEN_DIR}/kube_codegen.sh"
 
 rm -f $GOPATH/bin/*-gen
 
-bash "${CODE_GEN_DIR}/generate-internal-groups.sh" \
-  deepcopy,defaulter,conversion \
-  github.com/23technologies/gardener-extension-provider-hcloud/pkg/client \
-  github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud \
-  github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud \
-  "apis:v1alpha1" \
-  --go-header-file "$GARDENER_HACK_DIR/LICENSE_BOILERPLATE.txt"
+CURRENT_DIR=$(dirname $0)
+PROJECT_ROOT="${CURRENT_DIR}"/..
 
-bash "${CODE_GEN_DIR}/generate-internal-groups.sh" \
-  deepcopy,defaulter,conversion \
-  github.com/23technologies/gardener-extension-provider-hcloud/pkg/client/config \
-  github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis \
-  github.com/23technologies/gardener-extension-provider-hcloud/pkg/hcloud/apis \
-  "config:v1alpha1" \
-  --go-header-file "$GARDENER_HACK_DIR/LICENSE_BOILERPLATE.txt"
+kube::codegen::gen_helpers \
+  --boilerplate "${GARDENER_HACK_DIR}/LICENSE_BOILERPLATE.txt" \
+  "${PROJECT_ROOT}/pkg/apis/openstack"
+
+kube::codegen::gen_helpers \
+  --boilerplate "${GARDENER_HACK_DIR}/LICENSE_BOILERPLATE.txt" \
+  "${PROJECT_ROOT}/pkg/apis/config"
